551656d2bbe2c6ec446be7ee49b9468fe4c53143
Ruslan Kuprieiev <kupruser@gmail.com>
2018-03-12 18:41:57 +0200

    Remove (py)graphviz from dependencies

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -7,16 +7,9 @@ class Dvc < Formula
 
   depends_on "pkg-config" => :build
   depends_on "python"
-  depends_on "graphviz"
-
-  resource "pygraphviz" do
-    url "https://files.pythonhosted.org/packages/98/bb/a32e33f7665b921c926209305dde66fe41003a4ad934b10efb7c1211a419/pygraphviz-1.3.1.tar.gz"
-    sha256 "7c294cbc9d88946be671cc0d8602aac176d8c56695c0a7d871eadea75a958408"
-  end
 
   def install
     venv = virtualenv_create(libexec)
-    venv.pip_install resources
     system libexec/"bin/pip", "install", "-r", "requirements.txt"
     venv.pip_install_and_link buildpath
   end
