473febc90633d4fe75e0d5a2f2a03a27a47183d0
Ruslan Kuprieiev <kupruser@gmail.com>
2018-03-20 23:10:53 +0200

    dvc: update to 0.9.0
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>

--- Casks/dvc.rb
+++ Casks/dvc.rb
@@ -2,7 +2,7 @@ cask 'dvc' do
   name 'Data Version Control'
   homepage 'https://dataversioncontrol.com'
   version '0.9.0'
-  url "https://github.com/dataversioncontrol/dvc/releases/download/#{version}-test-3/dvc-#{version}.pkg"
+  url "https://github.com/dataversioncontrol/dvc/releases/download/#{version}/dvc-#{version}.pkg"
   sha256 :no_check
 
   pkg "dvc-#{version}.pkg"
