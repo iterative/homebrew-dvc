e7a3221042f1b016ad74b6fbcd53a75eb7465704
Ruslan Kuprieiev <kupruser@gmail.com>
2018-05-30 15:17:06 +0300

    cask: dataversioncontrol -> iterative

--- Casks/dvc.rb
+++ Casks/dvc.rb
@@ -2,7 +2,7 @@ cask 'dvc' do
   name 'Data Version Control'
   homepage 'https://dataversioncontrol.com'
   version '0.9.7'
-  url "https://github.com/dataversioncontrol/dvc/releases/download/#{version}/dvc-#{version}.pkg"
+  url "https://github.com/iterative/dvc/releases/download/#{version}/dvc-#{version}.pkg"
   sha256 :no_check
 
   pkg "dvc-#{version}.pkg"
