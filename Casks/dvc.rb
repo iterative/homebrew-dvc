96ed8e77e6a3bbfc0bb01b62b4ae7146c2ff6ed6
Ruslan Kuprieiev <kupruser@gmail.com>
2018-11-06 19:24:31 +0200

    cask: update to 0.20.1

--- Casks/dvc.rb
+++ Casks/dvc.rb
@@ -1,7 +1,7 @@
 cask 'dvc' do
   name 'Data Version Control'
   homepage 'https://dataversioncontrol.com'
-  version '0.20.0'
+  version '0.20.1'
   url "https://github.com/iterative/dvc/releases/download/#{version}/dvc-#{version}.pkg"
   sha256 :no_check
 
