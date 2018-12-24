ed15f7093f12549eeda005e5ae77e8ecf541d8f7
Ruslan Kuprieiev <kupruser@gmail.com>
2018-12-24 17:51:40 +0200

    formula: update to 0.23.1

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -3,7 +3,7 @@ class Dvc < Formula
 
   desc "Git for data science projects"
   homepage "https://dataversioncontrol.com/"
-  url "https://github.com/iterative/dvc/archive/0.23.0.tar.gz"
+  url "https://github.com/iterative/dvc/archive/0.23.1.tar.gz"
 
   depends_on "pkg-config" => :build
   depends_on "python"
