c87fef79a8f9f8f50a18cebb412a88e5e55679a3
Ruslan Kuprieiev <kupruser@gmail.com>
2019-01-03 14:42:59 +0200

    formula: expand workaround comment

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -14,8 +14,9 @@ class Dvc < Formula
     # NOTE: dvc depends on asciimatics, which depends on Pillow, which
     # uses liblcms2.2.dylib that causes troubles on mojave. See
     # https://github.com/iterative/homebrew-dvc/issues/9 for more info.
-    # As a workaround, we need to simply uninstall Pillow before our
-    # next step.
+    # As a workaround, we need to simply uninstall Pillow and replace
+    # asciimatics with our own patched version, that doesn't require
+    # Pillow.
     system libexec/"bin/pip", "uninstall", "-y", "asciimatics"
     system libexec/"bin/pip", "uninstall", "-y", "Pillow"
     system libexec/"bin/pip", "install", "git+https://github.com/efiop/asciimatics"
