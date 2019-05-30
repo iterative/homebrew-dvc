f6a05e84c7cd785358790ffa66b6d1f7547696a7
Ruslan Kuprieiev <kupruser@gmail.com>
2019-05-30 05:41:51 +0300

    formula: fix 0.41.3 build

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -3,14 +3,14 @@ class Dvc < Formula
 
   desc "Git for data science projects"
   homepage "https://dataversioncontrol.com/"
-  url "https://github.com/iterative/dvc/archive/0.41.0.tar.gz"
+  url "https://github.com/iterative/dvc/archive/0.41.3.tar.gz"
 
   depends_on "pkg-config" => :build
   depends_on "python"
 
   def install
     venv = virtualenv_create(libexec)
-    system libexec/"bin/pip", "install", "-r", "requirements.txt"
+    system libexec/"bin/pip", "install", ".[all]"
     # NOTE: dvc depends on asciimatics, which depends on Pillow, which
     # uses liblcms2.2.dylib that causes troubles on mojave. See
     # https://github.com/iterative/homebrew-dvc/issues/9 for more info.
@@ -20,7 +20,7 @@ class Dvc < Formula
     system libexec/"bin/pip", "uninstall", "-y", "asciimatics"
     system libexec/"bin/pip", "uninstall", "-y", "Pillow"
     system libexec/"bin/pip", "install", "git+https://github.com/efiop/asciimatics"
-    venv.pip_install_and_link buildpath
+    bin.install_symlink libexec/"bin/dvc"
   end
 
   test do
