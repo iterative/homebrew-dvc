d4beb095e50f5cbf885825b80f7c4891bb75d101
Ruslan Kuprieiev <kupruser@gmail.com>
2019-10-23 02:01:58 +0300

    formula: bring back Pillow workaround

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -11,6 +11,14 @@ class Dvc < Formula
   def install
     venv = virtualenv_create(libexec)
     system libexec/"bin/pip", "install", ".[all]"
+    # NOTE: dvc depends on asciimatics, which depends on Pillow, which
+    # uses liblcms2.2.dylib that causes troubles on mojave. See [1]
+    # and [2] for more info. As a workaround, we need to simply
+    # uninstall Pillow.
+    #
+    # [1] https://github.com/peterbrittain/asciimatics/issues/95
+    # [2] https://github.com/iterative/homebrew-dvc/issues/9
+    system libexec/"bin/pip", "uninstall", "-y", "Pillow"
     bin.install_symlink libexec/"bin/dvc"
   end
 
