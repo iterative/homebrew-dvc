5f09544c580aeba5cba93269b9bd79be5074b579
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-18 00:54:27 +0200

    Update dvc.rb

--- Formula/dvc.rb
+++ Formula/dvc.rb
@@ -7,7 +7,7 @@ class Dvc < Formula
   sha256 "a072ebf2151213c61ac7e580e51dc1cecefa4cd840e4f7ae1927d6710312cfe0"
 
   depends_on "pkg-config" => :build
-#  depends_on "cython" => :build
+  depends_on "cython" => :build
   depends_on "openssl@1.1"
   depends_on "python"
 
@@ -16,8 +16,8 @@ class Dvc < Formula
     File.open("dvc/utils/build.py", "w+") { |file| file.write("PKG = \"brew\"") }
 
     # NOTE: required for building pyarrow
-#    version = Language::Python.major_minor_version "python3"
-#    ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{version}/site-packages"
+    version = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{version}/site-packages"
 
     system libexec/"bin/pip", "install", "Pillow"
     system libexec/"bin/pip", "install", "--no-binary", ":all:", ".[all]"
